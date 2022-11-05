bfabfe590ca3e24c40a72644e46985b4ad6eea52
Allison Ause <allison.ause@gmail.com>
2022-11-05 11:56:36 -0700

    added error handling to custom hook

--- src/hooks/useCats.js
+++ src/hooks/useCats.js
@@ -6,11 +6,18 @@ export default function useCats() {
   const [cats, setCats] = useState([]);
   useEffect(() => {
     const fetchData = async () => {
-      const data = await fetchCats();
-      console.log('data, data');
-      setCats(data);
+      try {
+        const data = await fetchCats();
+        console.log('data', data);
+        setCats(data);
+      } catch (e) {
+        console.log(e);
+      }
     };
     fetchData();
   }, []);
   return { cats };
 }
+
+// I should put my error handling in here.
+// The Try / Catch should go into this function where we CALL the fetch!
